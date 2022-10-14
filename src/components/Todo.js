cc275eb978b8fed2a59c9b6a526de73e377ed22a
Kaung Myat Tun <kaungtun111@gmail.com>
2022-10-14 23:40:48 +0630

    delete operations

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,25 +1,19 @@
 import React from 'react'
 
-const Todo = ({ todos, setTodos }) => {
-  const todoDelete = (id) => {
-    setTodos([...todos, todos.filter((todo) => todo.id != id)])
+const Todo = ({ todo, todos, setTodos }) => {
+  const todoDelete = () => {
+    setTodos(todos.filter((todoItem) => todoItem.id !== todo.id))
   }
   return (
-    <>
-      {todos.map((todo) => {
-        return (
-          <div className='todoContainer' key={todo.id}>  
-            <ul className='todolist'>
-              <li className='todoText'>{todo.text}</li>
-            </ul>
-            <div className="btnGroup">
-              <button className="todoCheck">check</button>
-              <button className="todoDelete" onClick={(id) => todoDelete(id)}>delete</button>
-            </div>
-          </div>
-        )
-      })}
-    </>   
+    <div className='todoContainer'>  
+      <ul className='todolist'>
+        <li className='todoText'>{todo.text}</li>
+      </ul>
+      <div className="btnGroup">
+        <button className="todoCheck">check</button>
+        <button className="todoDelete" onClick={() => todoDelete(todos.id)}>delete</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Todo from './Todo'
 
 const TodoList = ({ todos, setTodos }) => {
-  return (
-      <Todo todos={todos} setTodos={setTodos} />
+  return (<>
+      {todos.map((todo) => {
+        return <Todo  key={todo.id} todos={todos} setTodos={setTodos} todo={todo} />
+      })}
+    </>
   )
 }
 
