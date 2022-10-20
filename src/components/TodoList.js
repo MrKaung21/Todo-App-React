5efe7c5d61dd02aab6d12a9a40110c1b872940e4
Kaung Myat Tun <kaungtun111@gmail.com>
2022-10-20 23:02:23 +0630

    adding some CSS

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Todo from './Todo'
 
 const TodoList = ({ todos, setTodos,editTodo, setEditTodo }) => {
+
   return (<>
       {todos.map((todo) => {
-        return <Todo  key={todo.id} todos={todos} setTodos={setTodos} todo={todo} setEditTodo={setEditTodo}/>
+        return <Todo  key={todo.id} todos={todos} setTodos={setTodos} todo={todo} setEditTodo={setEditTodo} />
       })}
     </>
   )
